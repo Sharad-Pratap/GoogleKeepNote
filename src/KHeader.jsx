f1795c21e4b6956e6b27ae120e6545ee0ed33fe7
Sharad-Pratap <sharadpratap40@gmail.com>
2022-07-19 14:24:58 +0530

    initial commit

---
+++ src/KHeader.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const KHeader = () =>{
+
+    return(
+        <>
+        <div className="header">
+        <img src="https://www.freepnglogos.com/uploads/camera-logo-png/photography-clipart-camera-logo-pencil-color-6.png" alt="logo" width="70" height="50"/>
+        <h1>Sharad Notes</h1>
+        </div>
+        </>
+    )
+}
+export default KHeader;
